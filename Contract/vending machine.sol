641fc92bf248eab57b69d51936da950e6ffa1318
Talha Javaid <106012677+Talha-Javaid@users.noreply.github.com>
2022-06-19 16:34:36 +0500

    Update vending machine.sol

--- Contract/vending machine.sol
+++ Contract/vending machine.sol
@@ -7,6 +7,11 @@ contract VendingMachine{
         uint public DonutPrice;
 
     mapping(address=>uint) public DonutsAmount;
+    
+    // set the owner as the address that deployed the contract
+    //set the price of one donut to 5 ether
+    // set the initial vending machine donuts to 100
+    
     constructor() {
         
         Owner=msg.sender;
@@ -14,6 +19,8 @@ contract VendingMachine{
         DonutsAmount[address(this)]=100;
     
     }
+    
+    //used to allow only owner of contract to perform tasks
 
     modifier onlyOwner() {
         
@@ -22,18 +29,27 @@ contract VendingMachine{
         _;
     
     }
+    
+    // only contract owner is allowed to update the price of donut
+    
     function UpdateDonutPrice(
         uint _UpdatedPrice) 
         public onlyOwner{
         DonutPrice=_UpdatedPrice;
     
     }
+    
+    // this function get the no of donuts currently present in vending machine
+    
     function getVendingMachineBalance() 
         public view returns(uint){
         return 
         DonutsAmount[address(this)];
     
     }
+    
+    // donuts are restocked here
+    
     function RestockDonuts(
         uint _AddAmount) 
         public onlyOwner 
@@ -41,6 +57,12 @@ contract VendingMachine{
         DonutsAmount[address(this)] += _AddAmount;
     
     }
+    
+    //here is the purchase function for donut
+    //amount of donuts must be greater than or equal to the donuts present
+    //price of donut paid by buyer must equal to or higher than the donut price
+    //
+    
     function PurchaseDonut(
         uint _PayAmount) 
         public payable
@@ -51,7 +73,8 @@ contract VendingMachine{
         require(msg.value>=DonutPrice,
         "Not enough amount to purchase donut");
         
-        require(DonutsAmount[address(this)] >= _PayAmount,"Currently donuts are out of stock wait for restock");
+        require(DonutsAmount[address(this)] >= _PayAmount,
+        "Currently donuts are out of stock wait for restock");
 
         DonutsAmount[address(this)] -= _PayAmount;
         
